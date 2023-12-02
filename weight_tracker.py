4aa3bfa3ab878a97d8ce761707d2a28883f6a7af
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-12-03 01:22:31 +0530

    Update weight_tracker.py

--- weight_tracker.py
+++ weight_tracker.py
@@ -6,29 +6,34 @@ import plotly.express as px
 
 def detail_about_your_weight(df):
     st.write(df)
+    # Ensure 'date' column is in datetime format
     df['date'] = pd.to_datetime(df['date'])
-    earliest_weight = df['weight'].iloc[-1] if not df.empty else None
-    latest_weight = df['weight'].iloc[0] if not df.empty else None
-    weight_change = latest_weight - earliest_weight if earliest_weight is not None and latest_weight is not None else None
-    if weight_change is not None:
-        percentage_change = (weight_change / earliest_weight) * 100 if earliest_weight != 0 else 0
-        if weight_change > 0:
-            st.write(f"Weight has increased by {weight_change:.2f} units.")
-            st.write(f"This is a {percentage_change:.2f}% increase since the earliest record.")
-        elif weight_change < 0:
-            st.write(f"Weight has decreased by {-weight_change:.2f} units.")
-            st.write(f"This is a {-percentage_change:.2f}% decrease since the earliest record.")
+
+    # Sort DataFrame by 'date' column in descending order
+    df = df.sort_values('date', ascending=False)
+
+    # Isolate the last two dates' weights
+    last_two_weights = df.head(2)['weight'].tolist()
+
+    if len(last_two_weights) == 2:
+        earliest_weight = last_two_weights[1]  # Second last weight
+        latest_weight = last_two_weights[0]  # Latest weight
+        weight_change = latest_weight - earliest_weight
+
+        if weight_change != 0:
+            percentage_change = (weight_change / abs(earliest_weight)) * 100 if earliest_weight != 0 else 0
+
+            if weight_change > 0:
+                st.write(f"Weight has increased by {weight_change:.2f} units.")
+                st.write(f"This is a {percentage_change:.2f}% increase since the second last record.")
+            else:
+                st.write(f"Weight has decreased by {-weight_change:.2f} units.")
+                st.write(f"This is a {-percentage_change:.2f}% decrease since the second last record.")
         else:
             st.write("Weight remains unchanged.")
     else:
-        st.write("Insufficient data to calculate weight change.")
-    six_months_ago = datetime.date.today() - pd.DateOffset(months=6)
-    filtered_df = df[df['date'] >= six_months_ago]
-    if not filtered_df.empty:
-        fig = px.line(filtered_df, x='date', y='weight', title='Weight Trends for Last 6 Months')
-        st.plotly_chart(fig)
-    else:
-        st.write("No data available for the last 6 months.")
+        st.write("Insufficient data to calculate weight change for the last two days.")
+
 
 
 
