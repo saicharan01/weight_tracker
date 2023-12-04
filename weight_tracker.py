ee5834234456a8fe843425f707f1fcfdc60ec21c
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-12-04 13:00:55 +0530

    Update weight_tracker.py

--- weight_tracker.py
+++ weight_tracker.py
@@ -1,23 +1,26 @@
 import streamlit as st
 import pandas as pd
 import json
-import datetime
 import plotly.express as px
+import datetime
 
 def detail_about_your_weight(df):
     st.write(df)
     # Ensure 'date' column is in datetime format
     df['date'] = pd.to_datetime(df['date'])
+    
+    # Convert 'weight' column to numeric type, handling non-convertible values as NaN
+    df['weight'] = pd.to_numeric(df['weight'], errors='coerce')
 
     # Sort DataFrame by 'date' column in descending order
-    df = df.sort_values('date', ascending=False)
+    df = df.sort_values('date', ascending=True)  # Ensure the data is sorted in ascending order for plotting
 
     # Isolate the last two dates' weights
-    last_two_weights = df.head(2)['weight'].tolist()
+    last_two_weights = df.tail(2)['weight'].tolist()  # Using tail(2) to get the last 2 records
 
     if len(last_two_weights) == 2:
-        earliest_weight = last_two_weights[1]  # Second last weight
-        latest_weight = last_two_weights[0]  # Latest weight
+        earliest_weight = last_two_weights[0]  # Second last weight
+        latest_weight = last_two_weights[1]  # Latest weight
         weight_change = latest_weight - earliest_weight
 
         if weight_change != 0:
@@ -33,12 +36,21 @@ def detail_about_your_weight(df):
             st.write("Weight remains unchanged")
         
         # Plotting weekly weight trend
-        weekly_trend = df.resample('W-Mon', on='date').mean().reset_index()  # Weekly average weight
-        fig = px.line(weekly_trend, x='date', y='weight', title='Weekly Weight Trend')
-        st.plotly_chart(fig)
+        
 
     else:
         st.write("Insufficient data to calculate weight change for the last two days.")
+
+    six_months_ago = datetime.date.today() - pd.DateOffset(months=6)
+    filtered_df = df[df['date'] >= six_months_ago]
+
+    # Visualization of weight trends for the last 6 months using Plotly
+    if not filtered_df.empty:
+        fig = px.line(filtered_df, x='date', y='weight', title='Weight Trends for Last 6 Months')
+        st.plotly_chart(fig)
+    else:
+        st.write("No data available for the last 6 months.")
+
 
 
 
